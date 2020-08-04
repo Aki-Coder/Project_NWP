93fe74b9a65b14f7f1b6ca79bbefb2af012c0adf
Aki-Coder <anchicabeybe@gmail.com>
2020-08-04 22:28:25 +0200

    adding autologout, clear localStorage for user

--- src/app/auth/logging/logging.service.ts
+++ src/app/auth/logging/logging.service.ts
@@ -23,6 +23,7 @@ export interface AuthResponseData{
 export class AuthService{
 
   user = new Subject<User>();
+  private tokenExpirationTimer: any;
 
   constructor(private http : HttpClient,
               private route : Router){}
@@ -82,12 +83,25 @@ export class AuthService{
 
     if(loadedUser.token){
       this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
   }
 
   logout(){
     this.user.next(null);
     this.route.navigate(['auth']);
+    localStorage.removeItem('userData');
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration : number){
+    this.tokenExpirationTimer = setTimeout(() =>{
+      this.logout();
+    },expirationDuration);
   }
 
 
@@ -101,6 +115,7 @@ export class AuthService{
     const user = new User(email,userId,token,expirationDate);
 
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
